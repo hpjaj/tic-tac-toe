247611cab90e7928cbc71ae3e2c32b9397d068a3
hpjaj <hmlevine@gmail.com>
2017-01-05 19:02:16 -0700

    Example of calling methods from other files

---
+++ board.rb
@@ -0,0 +1,11 @@
+class Board
+  # class method
+  def self.style
+    "paper"
+  end
+
+  # instance method
+  def level
+    "easy"
+  end
+end
---
+++ player.rb
@@ -0,0 +1,37 @@
+# 1 - requiring another file enables you to its methods in this file
+# 2 - you can exclude the .rb from the file name
+# 3 - "./" means the file is located in the same directory as this one is
+require "./board"
+
+class Player
+  # optional initialize method
+  def initialize(player_count)
+    @player_count = player_count
+  end
+
+  def double_players
+    @player_count * 2
+  end
+
+  # calling a class method from another file
+  def board_material
+    Board.style
+  end
+
+  # calling an instance method from another file
+  def board_difficulty
+    Board.new.level
+  end
+end
+
+
+# the commands you want to execute
+new_players = Player.new(2)
+p new_players.double_players
+p new_players.board_material
+p new_players.board_difficulty
+
+
+# in order to run this "program":
+# 1 - cd into the tic-tac-toe directory
+# 2 - run `ruby player.rb`
